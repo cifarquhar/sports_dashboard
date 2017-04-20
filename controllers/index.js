c71c671e757494cc918f590efdcf7d44b856b5ac
Colin Farquhar <cifarquhar@gmail.com>
2017-04-20 15:58:11 +0100

    fix api key issue, add league_table.html

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + "/../client/index.html"))
 })
 
+router.get('/table', function(req, res) {
+  res.sendFile(path.join(__dirname + "/../client/league_table.html"))
+})
+
 module.exports = router
