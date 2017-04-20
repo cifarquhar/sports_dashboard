569b974674bfd050ab9135bdb95aee142e786e5f
David Hale <dhale12@hotmail.co.uk>
2017-04-20 14:44:44 +0100

    create all function for favouritesQuery

--- db/favourites_query.js
+++ db/favourites_query.js
@@ -4,3 +4,19 @@ var FavouritesQuery = function(){
   this.url = 'mongodb://localhost:27017/sports_dashboard'
 }
 
+FavouritesQuery.prototype = {
+
+  all: function(onQueryFinished) {
+    MongoClient.connect(this.url, function(err, db) {
+      if(db) {
+        var collection = db.collection('favourites')
+        collection.find().toArray(function(err, data) {
+          onQueryFinished(data)
+        })
+      }
+    })
+  }
+
+}
+
+module.exports = FavouritesQuery
