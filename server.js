a03d1af3e108968b123867f49f906677eb921627
Colin Farquhar <cifarquhar@gmail.com>
2017-04-20 14:16:07 +0100

    add content to server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express")
+var app = express()
+
+app.use(require("controllers/index.js"))
+
+app.use(express.static("client/build"))
+
+app.listen(3000,function(){
+  console.log("Awesome Brogrammers stuff happening!")
+})
