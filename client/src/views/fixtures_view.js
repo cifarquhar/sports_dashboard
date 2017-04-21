9593b33820f2c56d8054b3c0e41662c9cf95dad7
David Hale <dhale12@hotmail.co.uk>
2017-04-21 14:06:45 +0100

    more refactoring of populateList in fixtures view

--- client/src/views/fixtures_view.js
+++ client/src/views/fixtures_view.js
@@ -9,28 +9,25 @@ FixturesView.prototype = {
     var fixturesArray = fixtures.fixtures
     for (var fixture of fixturesArray) {
       var li = document.createElement('li')
-      
-      var pDate = this.createPtag('date', li)
-      pDate.innerText = fixture.date
-
-      var pHomeTeam = this.createPtag('home-team', li)
-      pHomeTeam.innerText = fixture.homeTeamName
-
-      var pAwayTeam = this.createPtag('away-team', li)
-      pAwayTeam.innerText = fixture.awayTeamName
-
-      var pResult = this.createPtag('result', li)
-      pResult.innerText = this.formatResult(fixture)
-
+      this.populateList(fixture, li)
       this.element.appendChild(li)
     }
   },
 
-  createPtag: function(id, li) {
+  createPtag: function(id, li, text) {
     var pTag = document.createElement('p')
     pTag.id = id
+    pTag.innerText = text
     li.appendChild(pTag)
     return pTag
+  },
+
+  populateList: function(fixture, li) {
+    var pDate = this.createPtag('date', li, fixture.date)
+    var pHomeTeam = this.createPtag('home-team', li, fixture.homeTeamName)
+    var pAwayTeam = this.createPtag('away-team', li, fixture.awayTeamName)
+    var result = this.formatResult(fixture)
+    var pResult = this.createPtag('result', li, result)
   },
 
   formatResult: function(fixture) {
