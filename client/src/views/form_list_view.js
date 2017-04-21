a67a242c862c030c99ddef07f6327f530f8e86b4
Colin Farquhar <cifarquhar@gmail.com>
2017-04-21 15:35:07 +0100

    add boxes for results to form view

--- client/src/views/form_list_view.js
+++ client/src/views/form_list_view.js
@@ -7,9 +7,18 @@ FormListView.prototype = {
   render: function(formList,element){
     console.log("stuff happening in the form view")
 
+    var fixtures = formList.fixtures
+    console.log(fixtures)
+
     while (element.hasChildNodes()) {
       element.removeChild(element.lastChild);
     }
+
+    fixtures.forEach(function(fixture){
+      var resultBox = document.createElement("div")
+      resultBox.style = "width:10px;height:10px;border:1px solid #000;"
+      element.appendChild(resultBox)
+    })
   }
 
 }
