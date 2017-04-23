2c5644b8353d8a90001beaaeccf041d6c59ca478
David Hale <dhale12@hotmail.co.uk>
2017-04-23 18:25:09 +0100

    wrote render function with welcome message for homepage

--- client/src/views/index_view.js
+++ client/src/views/index_view.js
@@ -1,5 +1,23 @@
 var IndexView = function() {
-  
+
+}
+
+IndexView.prototype = {
+
+  render: function() {
+    var body = document.querySelector('body')
+    var h2 = document.createElement('h2')
+    var pTag1 = document.createElement('p')
+    var pTag2 = document.createElement('p')
+
+    h2.innerText = 'Welcome to the Sports Dashboard!'
+    pTag1.innerText = 'Click the links above to see upcoming fixtures, team details and add fixtures to your watchlist.'
+    pTag2.innerText = 'It\'s just football for now, but we\'ll have more sports info coming at you very soon.'
+    body.appendChild(h2)
+    body.appendChild(pTag1)
+    body.appendChild(pTag2)
+  }
+
 }
 
 module.exports = IndexView
