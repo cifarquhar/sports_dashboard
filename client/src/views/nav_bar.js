9a17c594cef745b25df77ec955a5888fc01b77a4
David Hale <dhale12@hotmail.co.uk>
2017-04-23 14:10:23 +0100

    created the method to make nav bar

--- client/src/views/nav_bar.js
+++ client/src/views/nav_bar.js
@@ -5,7 +5,14 @@ var NavBar = function() {
 NavBar.prototype = {
 
   createNavBar: function() {
-
+    var nav = document.createElement('nav')
+    var ol = document.createElement('ol')
+    var fixturesLi = createLi('Fixtures', 'http://localhost:3000/map')
+    var favouritesLi = createLi('Favourites', 'http://localhost:3000/favourites')
+    ol.appendChild(fixturesLi)
+    ol.appendChild(favouritesLi)
+    nav.appendChild(ol)
+    this.div.appendChild(nav)
   },
 
   createLi: function(label, link) {
