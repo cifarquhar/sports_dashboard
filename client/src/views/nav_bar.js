7347fe5463b6d99fd191e7973ab6b3b8da1e02b5
David Hale <dhale12@hotmail.co.uk>
2017-04-23 14:04:43 +0100

    wrote createLi method for nav bar

--- client/src/views/nav_bar.js
+++ client/src/views/nav_bar.js
@@ -2,4 +2,21 @@ var NavBar = function() {
   this.div = document.querySelector('#nav-bar')
 }
 
+NavBar.prototype = {
+
+  createNavBar: function() {
+
+  },
+
+  createLi: function(label, link) {
+    var li = document.createElement('li')
+    var a = document.createElement('a')
+    a.href = link
+    a.innerText = label
+    li.appendChild(a)
+    return li
+  },
+
+}
+
 module.exports = NavBar
