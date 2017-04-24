02f841a174526d7668894a34f003debd44923032
David Hale <dhale12@hotmail.co.uk>
2017-04-24 13:48:33 +0100

    refactoring

--- client/src/views/team_stats_view.js
+++ client/src/views/team_stats_view.js
@@ -15,66 +15,59 @@ TeamStatsView.prototype = {
   render: function(league){
 
     this.teamSelector = document.querySelector("#team-selector")
-    var playerElement = this.playerElement
-    var formElement = this.formElement
     var matchElement = this.matchElement
-    var formOption = this.formOption
-    
+    var pChooseTeam = document.createElement('p')
     var teams = league.teams.sort()
-    var teamName = teams[0].name
 
+    this.addChooseTeamText('Select a team:')
+    this.generateOptions(teams)
+    this.renderSquadList(teams[0]._links.players.href, this.playerElement)
+    this.renderTeamForm(this.formElement, teams[0].name, this.formOption, teams[0]._links.fixtures.href)
+    
+
+    this.teamSelector.addEventListener("change",function(){
+      
+      this.renderSquadList(teams[this.teamSelector.value]._links.players.href, this.playerElement)
+
+      this.renderTeamForm(this.formElement, teams[this.teamSelector.value].name, this.formOption, teams[this.teamSelector.value]._links.fixtures.href)
+
+      while (matchElement.hasChildNodes()) {
+        matchElement.removeChild(matchElement.lastChild);
+      }
+    }.bind(this))
+
+  },
+
+  addChooseTeamText: function(fillerText) {
+    var body = document.querySelector('body')
+    var pChooseTeam = document.createElement('p')
+    pChooseTeam.innerText = fillerText
+    body.appendChild(pChooseTeam)
+  },
+
+  generateOptions: function(teams) {
     teams.forEach(function(team,index){
       var teamOption = document.createElement("option")
       teamOption.innerText = team.name
       teamOption.value = index
       this.teamSelector.appendChild(teamOption)
     }.bind(this))
+  },
 
-    var teamPlayerURL = teams[0]._links.players.href
+  renderSquadList: function(teamPlayerURL, playerElement, squadList) {
     var teamPlayers = new PlayerList(teamPlayerURL)
     var teamPlayerView = new PlayerListView()
     teamPlayers.getData(function(squadList){
       teamPlayerView.render(squadList,playerElement)
     })
+  },
 
-
-    var teamFormURL = teams[0]._links.fixtures.href
-    var teamForm = new FormList(teamFormURL)
+  renderTeamForm: function(formElement, teamName, formOption, fixtures) {
+    var teamForm = new FormList(fixtures)
     var teamFormView = new FormListView()
     teamForm.getData(function(fixtures){
-      teamFormView.render(fixtures,formElement,teamName,formOption)
-    })
-
-
-
-
-
-
-
-   this.teamSelector.addEventListener("change",function(){
-      var teamPlayerURL = teams[this.value]._links.players.href
-      var teamPlayers = new PlayerList(teamPlayerURL)
-      teamPlayers.getData(function(squadList){
-        teamPlayerView.render(squadList,playerElement)
-      }.bind(this))
-      var newTeamName = teams[this.value].name
-      var teamFormURL = teams[this.value]._links.fixtures.href
-      var teamForm = new FormList(teamFormURL)
-      teamForm.getData(function(fixtures){
-        teamFormView.render(fixtures,formElement,newTeamName,formOption)
-      })
-      while (matchElement.hasChildNodes()) {
-        matchElement.removeChild(matchElement.lastChild);
-      }
-    })
-
-   
-
-
-
-    
-    
-
+      teamFormView.render(fixtures, formElement, teamName, formOption)
+    }.bind(this))
   }
 
 }
