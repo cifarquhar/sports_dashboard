b20b81ad92781c81e43b8e7e90aef8336909d228
David Hale <dhale12@hotmail.co.uk>
2017-04-25 11:57:25 +0100

    some pseudocode for aside render

--- client/src/views/aside_table.js
+++ client/src/views/aside_table.js
@@ -2,3 +2,25 @@ var AsideTable = function() {
   this.asideElement = document.createElement('aside')
   this.asideElement.id = ('aside-table')
 }
+
+AsideTable.prototype = {
+
+  render: function() {
+    // Make table
+    var table = document.createElement('table')
+    // Make header row
+    // Make headers 
+    // Append headers to row
+    // Append row to table
+    // Make team entry row
+    // Add position
+    // Add team name add points
+    // Append position name and points to row
+    // Append row to table
+  }
+
+  createHeader: function(row) {
+
+  }
+
+}
