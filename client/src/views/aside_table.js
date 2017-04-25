806a88bfe7567f8f69f39cd72ab2a4a2605fd5d0
David Hale <dhale12@hotmail.co.uk>
2017-04-25 12:05:33 +0100

    wrote createEntryCell and createEntryRow

--- client/src/views/aside_table.js
+++ client/src/views/aside_table.js
@@ -15,19 +15,36 @@ AsideTable.prototype = {
     createHeader(headerRow, 'Position')
     createHeader(headerRow, 'Team')
     createHeader(headerRow, 'Points')
-    
     // Append row to table
+    table.appendChild(headerRow)
     // Make team entry row
+
     // Add position
     // Add team name add points
     // Append position name and points to row
     // Append row to table
-  }
+  },
 
   createHeader: function(row, label) {
     var header = document.createElement('th')
     header.innerText = label
     row.appendChild(header)
-  }
+  },
+
+  createEntryCell: function(data, row) {
+    var cell = document.createElement('td')
+    cell.innerText = data
+    row.appendChild(cell)
+  },
+
+  createEntryRow: function(team, table) {
+    var row = document.createElement('tr')
+    createEntryCell(team.position, row)
+    createEntryCell(team.teamName, row)
+    createEntryCell(team.points, row)
+    table.appendChild(row)
+  },
+
+  
 
 }
