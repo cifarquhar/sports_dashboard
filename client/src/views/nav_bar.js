65a7657290371058ecaecef7c709f13353806b3d
David Hale <dhale12@hotmail.co.uk>
2017-04-26 11:23:32 +0100

    changed nav bar to say watchlist

--- client/src/views/nav_bar.js
+++ client/src/views/nav_bar.js
@@ -10,7 +10,7 @@ NavBar.prototype = {
     var nav = document.createElement('nav')
     var homeDiv = this.createNavLink('Home', 'http://localhost:3000')
     var fixturesDiv = this.createNavLink('Fixtures', 'http://localhost:3000/map')
-    var favouritesDiv = this.createNavLink('Favourites', 'http://localhost:3000/favourites')
+    var favouritesDiv = this.createNavLink('WatchList', 'http://localhost:3000/favourites')
     var tableDiv = this.createNavLink('Table', 'http://localhost:3000/table')
     var teamDiv = this.createNavLink('Team', 'http://localhost:3000/team')
     nav.appendChild(homeDiv)
